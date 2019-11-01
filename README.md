44254d9416f9cf9d10ed7444db0e1f47bfa38491
Dat Dao <datagit@yahoo.com>
2019-11-01 17:26:56 +0700

    seconds

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# My noted
+- https://www.taniarascia.com/getting-started-with-react/
+- CSS Primitive UI | MIT License: https://taniarascia.github.io/primitive/css/main.css
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
@@ -66,6 +71,3 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `yarn build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
-
-### CSS
-Primitive UI | MIT License: https://taniarascia.github.io/primitive/css/main.css
--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react'
 // import logo from './logo.svg';
 import './App.css';
 import Table from "./Table";
+import Form from "./Form";
 
-function App() {
-  return (
-      <div className="container">
-        <Table/>
-      </div>
-  );
+class App extends Component {
+  state = {
+    characters: [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+      {
+        name: 'Dat Dao',
+        job: 'IT',
+      },
+    ],
+  }
+  removeCharacter = index => {
+    const { characters } = this.state;
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      })
+    })
+  }
+  render() {
+    const { characters } = this.state
+    return (
+        <div className="container">
+          <Table characterData={characters} removeCharacter={this.removeCharacter}/>
+          <Form />
+        </div>
+    )
+  }
 }
 
 export default App;
